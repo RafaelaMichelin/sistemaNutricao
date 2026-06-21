0166928fd02fead291f13a6ea09de83ae1b0d248
RafaelaMichelin <RafaelaMichelin@gmail.com>
2026-06-21 15:24:58 -0300

    Feat: Adicionando Swagger e o EnableCors no Main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //swagger
+  const config = new DocumentBuilder()
+                     .setTitle('Api Sistema nutrição')
+                     .setDescription('Api voltado para um sistema de nutrição')
+                     .setVersion('1.0')
+                     .addTag('Swagger')
+                     .build();
+
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api-swagger', app, documentFactory);
+    
+  //LIBERAR CORS 
+     app.enableCors({
+      origin: 'localhost:3000', //URL DO FRONT-END
+      methods: ['GET', 'POST']  //MÉTODOS QUE SERÃO LIBERADORS PARA O FRONT
+});
     // Pipes
     app.useGlobalPipes(
     new ValidationPipe({
